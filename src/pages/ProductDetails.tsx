5b782cbe6e7332b8f494da1f3926e7c0493168b3
Jovicy <johnvictordml@gmail.com>
2026-05-07 14:38:10 +0100

    updated

--- src/pages/ProductDetails.tsx
+++ src/pages/ProductDetails.tsx
@@ -202,7 +202,7 @@ export default function ProductDetail() {
 
 
             <button
-              onClick={() => navigate("/plan")}
+              onClick={() => navigate("/plans")}
               className="inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 hover-elevate active-elevate-2 border border-color:var(--button-outline) shadow-xs active:shadow-none min-h-9 px-4 py-2 flex-1 h-12 border-primary/50 text-primary hover:bg-primary/10">
               <LuSparkles />
               Join Payment Plan
